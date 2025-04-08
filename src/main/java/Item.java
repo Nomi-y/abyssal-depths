1f90e3dde078459b4f00177bc89b00239b07dd14
Nomi <simonfragner@neuerfokus.at>
2025-04-08 23:13:03 +0200

    syntax

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,32 +1,35 @@
-import java.util.Comparator;
-
-public class Item extends Entity implements Collectible, Comparaable<Item> {
+public class Item extends Entity implements Collectible, Comparable<Item> {
 
     private final String name;
     private final Rarity rarity;
-    
+
     public Item(String name, Rarity r) throws EntityException {
         super(0, 0, 1, 20);
-        if (name == null || name.isEmpty()) throw new EntityException("Item must have a name!");
+        if (name == null || name.isEmpty())
+            throw new EntityException("Item must have a name!");
         this.name = name;
         rarity = r;
     }
-    
-    public String getName() { return name; }
-    public Rarity getRarity() { return rarity; }
-    
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public Rarity getRarity() {
+        return rarity;
+    }
+
     @Override
-    public void levelUp() {}
-    
+    public void levelUp() {
+    }
+
     public int compareTo(Item other) {
         return this.getName().compareToIgnoreCase(other.getName()); // or compare by rarity
     }
 
     @Override
     public String toString() {
-    return "Item [Name=" + name + ", Rarity=" + rarity + "]";
+        return "Item [Name=" + name + ", Rarity=" + rarity + "]";
     }
 
-    
 }
